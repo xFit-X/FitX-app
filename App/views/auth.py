68b75a63fb12f24d43f01022f1eb06457498205f
Nicholas Mendez <snickdx@gmail.com>
2023-03-27 15:42:03 -0400

    Initial commit

---
+++ App/views/auth.py
@@ -0,0 +1,40 @@
+from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
+from flask_jwt_extended import jwt_required, current_user
+
+from.index import index_views
+
+from App.controllers import (
+    create_user,
+    authenticate, 
+)
+
+auth_views = Blueprint('auth_views', __name__, template_folder='../templates')
+
+@auth_views.route('/users', methods=['GET'])
+def get_user_page():
+    users = get_all_users()
+    return render_template('users.html', users=users)
+
+@auth_views.route('/api/users', methods=['GET'])
+def get_users_action():
+    users = get_all_users_json()
+    return jsonify(users)
+
+@auth_views.route('/api/users', methods=['POST'])
+def create_user_endpoint():
+    data = request.json
+    create_user(data['username'], data['password'])
+    return jsonify({'message': f"user {data['username']} created"})
+
+@auth_views.route('/api/login', methods=['POST'])
+def user_login_api():
+  data = request.json
+  token = authenticate(data['username'], data['password'])
+  if not token:
+    return jsonify(message='bad username or password given'), 401
+  return jsonify(access_token=token)
+
+@auth_views.route('/api/identify', methods=['GET'])
+@jwt_required()
+def identify_user_action():
+    return jsonify({'message': f"username: {current_user.username}, id : {current_user.id}"})
