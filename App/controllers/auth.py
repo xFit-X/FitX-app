14888a7337ce59798d1e745ecbcc2eb925d00d36
Nicholas Mendez <snickdx@gmail.com>
2023-03-28 13:00:16 +0000

    added initialze() controller

--- App/controllers/auth.py
+++ App/controllers/auth.py
@@ -1,5 +1,8 @@
 from flask_login import login_user
-from App.models import User, Staff, Customer
+from App.models import db, User, Staff, Customer, listing
+from .game import create_game
+from .user import create_customer, create_staff
+from .listing import list_game
 
 def login(username, password):
     staff = Staff.query.filter_by(username=username).first()
@@ -9,4 +12,17 @@ def login(username, password):
     if customer and customer.check_password(password):
         return customer
     return None
+
+def initialize():
+    db.drop_all()
+    db.create_all()
+    rob = create_customer('rob', 'robpass')
+    bob = create_staff('bob', 'bobpass')
+    game1 = create_game('my game', 12324)
+    game2 = create_game('my game2', 12325)
+    game3 = create_game('my game3', 12326)
+    list_game(bob.id, rob.id, game1.gameId, 'ok', 11)
+    list_game(bob.id, rob.id, game2.gameId, 'good', 12)
+    list_game(bob.id, rob.id, game3.gameId, 'new', 13)
+    
 
--- App/views/index.py
+++ App/views/index.py
@@ -1,9 +1,21 @@
-from flask import Blueprint, redirect, render_template, request, send_from_directory
+from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
-from App.controllers import (get_all_listings)
+from App.controllers import (
+    get_all_listings,
+    initialize,
+)
 
 @index_views.route('/', methods=['GET'])
 def index_page():
     listings = get_all_listings()
     return render_template('index.html', listings=listings)
+
+@index_views.route('/init', methods=['GET'])
+def init():
+    initialize()
+    return jsonify(message='db initialized!')
+
+@index_views.route('/health', methods=['GET'])
+def health_check():
+    return jsonify({'status':'healthy'})
