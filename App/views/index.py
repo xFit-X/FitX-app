2a579c641bfb4a16133424255ba635e3730af51a
Nicholas Mendez <snickdx@gmail.com>
2023-03-28 05:16:41 +0000

    added UI

--- App/views/index.py
+++ App/views/index.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
+from App.controllers import (get_all_listings)
 
 @index_views.route('/', methods=['GET'])
 def index_page():
-    return render_template('index.html')
+    listings = get_all_listings()
+    return render_template('index.html', listings=listings)
