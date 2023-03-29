8b352187f25a91cbefe01306c4323bbdb335319b
Nicholas Mendez <snickdx@gmail.com>
2023-03-29 10:16:41 -0400

    Update config.py

--- App/config.py
+++ App/config.py
@@ -7,7 +7,7 @@ def load_config():
     config = {'ENV': os.environ.get('ENV', 'DEVELOPMENT')}
     delta = 7
     if config['ENV'] == "DEVELOPMENT":
-        from .custom_config import JWT_ACCESS_TOKEN_EXPIRES, SQLALCHEMY_DATABASE_URI, SECRET_KEY, RAWG_TOKEN
+        from .default_config import JWT_ACCESS_TOKEN_EXPIRES, SQLALCHEMY_DATABASE_URI, SECRET_KEY, RAWG_TOKEN
         config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
         config['SECRET_KEY'] = SECRET_KEY
         config['RAWG_TOKEN'] = RAWG_TOKEN
